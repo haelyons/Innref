5ecbce5dba8fa59ce22081e0fec29dcab144ac25
Helios Lyons <helioslyons@me.com>
2022-08-02 10:52:49 +0200

    Use URL structure to feed parsing logic Add config for VSC debugging Better comments

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ import regex
 """ 
 Hélios Lyons
 30/7/22
-Innref [change to WanderingStat] (main.py)
+Innref [-> WanderingStat] (main.py)
 
 To-do:-
-1. WIP: Read TOC and create key-value pairs
-2. Fetch url from TOC data-structure (equivalent of structs in python?)
+1. WIP: Read TOC into dynamic data structure (python is fucking confusing)
+2. WIP: Fetch url from TOC data-structure (equivalent of structs in python?)
 3. WIP: Extract text from limited body (only chapter content)
 
 Documentation:
@@ -21,39 +21,41 @@ https://docs.google.com/document/d/12S1_J-qbng38_hZ9PT89PKkrwl4sMXRMc2Epe10xWfQ/
 
 """
 # To-do TOC: Add logic to sort chapters in TOC into key value pairs with URL
+
+# Open TOC, read and neaten
 page = urlopen('https://wanderinginn.com/table-of-contents/').read()
 soup = BeautifulSoup(page)
 soup.prettify()
 
-tableOfContents = []
+# TOC structure, URL pattern, counter
+TOC = [] 
 date_regex = r"(\d{4}/\d{2}/\d{2})"
 regexURL = "http://wanderinginn.com/2017/07/04/2-34/"
 url_regex = re.compile("(\d{4}/\d{2}/\d{2})")
 counter = 0
 
+# Iterate through TOC and collect chapter URLS
 for anchor in soup.findAll('a', href=True):
-    print(anchor['href'])
-    tableOfContents.append(anchor['href'])
+    anchor['href']
+    TOC.append(anchor['href'])
     counter += 1
 
-sortedContents = list(filter(url_regex.search, tableOfContents))
-print(sortedContents)
+# Only take URLS with dates (chapters, glossary) -- most recent is duplicate
+sortedTOC = list(filter(url_regex.search, TOC))
+#print(sortedTOC)
 print(counter)
 
 # Get url find page title
-url = "http://wanderinginn.com/2017/07/04/2-34/"
+url = sortedTOC[5]
 chapter_page = urlopen(url)
 html_bytes = chapter_page.read()
 html = html_bytes.decode("utf-8")
 soup2 = BeautifulSoup(html, "html.parser")
-# <article class ="post-1910 page type-page status-publish hentry" id="post-1910"> = $0
-
-# To-do URL: Logic for fetching url from TOC data structure
 
 # Extract chapter code (1.03)
 title = soup2.title.string
 chapter_code = re.findall("\d+\.\d+", title)
-#print(chapter_code)
+print(chapter_code)
 
 # Extract class references
 body = soup2.get_text()
