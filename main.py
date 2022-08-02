8e170e35b50ec03ab54e8734c62b15ece0699112
Helios Lyons <helioslyons@me.com>
2022-08-02 11:48:55 +0200

    Restructure for legibility and dev Logic for selecting chapter analysis Delimiting title

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from ast import parse
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from matplotlib.pyplot import table
-import mechanicalsoup
 import re
+from py import process
 import regex
 
 """ 
@@ -11,53 +10,58 @@ Hélios Lyons
 30/7/22
 Innref [-> WanderingStat] (main.py)
 
-To-do:-
-1. WIP: Read TOC into dynamic data structure (python is fucking confusing)
-2. WIP: Fetch url from TOC data-structure (equivalent of structs in python?)
-3. WIP: Extract text from limited body (only chapter content)
-
-Documentation:
-https://docs.google.com/document/d/12S1_J-qbng38_hZ9PT89PKkrwl4sMXRMc2Epe10xWfQ/edit 
-
 """
-# To-do TOC: Add logic to sort chapters in TOC into key value pairs with URL
 
-# Open TOC, read and neaten
-page = urlopen('https://wanderinginn.com/table-of-contents/').read()
-soup = BeautifulSoup(page)
-soup.prettify()
-
-# TOC structure, URL pattern, counter
+# GLOBAL VARIABLES
 TOC = [] 
 date_regex = r"(\d{4}/\d{2}/\d{2})"
-regexURL = "http://wanderinginn.com/2017/07/04/2-34/"
 url_regex = re.compile("(\d{4}/\d{2}/\d{2})")
-counter = 0
+urlTOC = 'https://wanderinginn.com/table-of-contents/'
+chapter = 400
 
-# Iterate through TOC and collect chapter URLS
-for anchor in soup.findAll('a', href=True):
-    anchor['href']
-    TOC.append(anchor['href'])
-    counter += 1
+def process_toc(url):
+    print("Beginning data processing...")
+    page = urlopen(urlTOC).read()
+    soup = BeautifulSoup(page)
+    soup.prettify()
 
-# Only take URLS with dates (chapters, glossary) -- most recent is duplicate
-sortedTOC = list(filter(url_regex.search, TOC))
-#print(sortedTOC)
-print(counter)
+    # Iterate through TOC and collect chapter URLS
+    for anchor in soup.findAll('a', href=True):
+        anchor['href']
+        TOC.append(anchor['href'])
 
-# Get url find page title
-url = sortedTOC[5]
-chapter_page = urlopen(url)
-html_bytes = chapter_page.read()
-html = html_bytes.decode("utf-8")
-soup2 = BeautifulSoup(html, "html.parser")
+    # Only take URLS with dates (chapters, glossary) -- most recent is duplicate
+    sortedTOC = list(filter(url_regex.search, TOC))
+    print(sortedTOC)
 
-# Extract chapter code (1.03)
-title = soup2.title.string
-chapter_code = re.findall("\d+\.\d+", title)
-print(chapter_code)
+    return sortedTOC
 
-# Extract class references
+# Add logic to delimit output (remove | Wandering Inn)
+def find_title(url):
+    # Get url find page title
+    chapter_page = urlopen(url)
+    html_bytes = chapter_page.read()
+    html = html_bytes.decode("utf-8")
+    soup2 = BeautifulSoup(html, "html.parser")
+
+    # Extract chapter code programatically
+    title = soup2.title.string
+    chapter_code = re.findall("\d+\.\d+", title)
+
+    delimited = title.split('|')
+
+    return delimited
+
+def main():
+    print("Entering main:")
+    sortedTOC = process_toc(urlTOC)
+    title = find_title(sortedTOC[chapter])
+    print(title)
+
+if __name__ == "__main__":
+    main()
+
+"""
 body = soup2.get_text()
 brackets = re.findall(r'\[.*?\]', body)
 #print(brackets)
@@ -69,3 +73,15 @@ level = soup2.find(string=re.compile("Level"))
 content = soup2.find_all(id='post-2226')
 # filteredContent = content.get_text()
 # print(content)'
+
+
+To-do:-
+1. DONE: Read TOC into dynamic data structure (python is fucking confusing)
+2. DONE: Fetch url from TOC data-structure (equivalent of structs in python?)
+3. DONE: Extract text from limited body (only chapter content)
+4. 
+
+Documentation:
+https://docs.google.com/document/d/12S1_J-qbng38_hZ9PT89PKkrwl4sMXRMc2Epe10xWfQ/edit 
+
+"""
